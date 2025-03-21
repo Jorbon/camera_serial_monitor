1395301192411e3690569ad83ad4cdc09deb836d
Jorbon <ben.a.phillips@outlook.com>
2025-03-21 17:05:04 -0500

    menu changes

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,12 @@ use simple_windows::{SimpleWindowApp, WindowHandle, Rect, Menu};
 
 
 fn main() {
-	let result = simple_windows::run_window_process("main", 320, 240, "Camera Serial Monitor", false, App::new());
-	
-	match result {
-		Ok(_) => {},
-		Err(err) => println!("{err}")
-	}
-	
+	simple_windows::run_window_process("main", 320, 240, "Camera Serial Monitor", false, App::new()).unwrap();
 }
+
+
+
+
 
 
 
@@ -22,7 +20,9 @@ pub struct App {
 	pub port: Option<Box<dyn SerialPort>>,
 	pub signal_x: u32,
 	pub signal_y: u32,
-	pub leftover_byte: Option<u8>,
+	pub href_match: usize,
+	pub vsync_match: usize,
+	pub first_byte_in_pair: bool,
 }
 
 impl App {
@@ -31,7 +31,9 @@ impl App {
 		port: None,
 		signal_x: 0,
 		signal_y: 0,
-		leftover_byte: None,
+		href_match: 0,
+		vsync_match: 0,
+		first_byte_in_pair: true,
 	}}
 	
 	pub fn build_menu(&mut self, handle: &WindowHandle) -> Result<(), String> {
@@ -39,8 +41,8 @@ impl App {
 		
 		let port_menu = Menu::new()?;
 		
-		for i in 1..=self.available_ports.len() {
-			port_menu.add_item(i as u16, &format!("{} - {}", self.available_ports[i-1].port_name, &match &self.available_ports[i-1].port_type {
+		for i in 0..self.available_ports.len() {
+			port_menu.add_item(i as u16, &format!("{} - {}", self.available_ports[i].port_name, &match &self.available_ports[i].port_type {
 				SerialPortType::UsbPort(info) => match info.product.as_ref() {
 					Some(s) => s,
 					None => match info.manufacturer.as_ref() {
@@ -56,8 +58,8 @@ impl App {
 		
 		if let Some(port) = self.port.as_ref() {
 			if let Some(name) = port.name() {
-				for i in 1..=self.available_ports.len() {
-					if self.available_ports[i-1].port_name == name {
+				for i in 0..self.available_ports.len() {
+					if self.available_ports[i].port_name == name {
 						port_menu.set_item_check(i as u16, true)?;
 						break
 					}
@@ -72,7 +74,8 @@ impl App {
 		
 		let menu = Menu::new()?;
 		menu.add_submenu(port_menu, "Select Port")?;
-		menu.add_item(0, "Rescan Ports")?;
+		menu.add_item(128, "Rescan Ports")?;
+		menu.add_item(129, "Disconnect")?;
 		handle.set_menu(menu)?;
 		
 		handle.redraw_menu()?;
@@ -84,29 +87,124 @@ impl App {
 			println!("Error creating menu: {err}");
 		}
 	}
+	
+	pub fn render_data(&mut self, handle: &WindowHandle, pixel_buffer: &mut [u8], client_rect: &Rect) {
+		let port = match &mut self.port {
+			None => return,
+			Some(port) => port
+		};
+		
+		let n = match port.bytes_to_read() {
+			Err(e) => {
+				println!("Serial read error: {e}");
+				return
+			}
+			Ok(0) => return,
+			Ok(n) => n
+		};
+		
+		let mut buf = vec![0; n as usize];
+		
+		match port.read(&mut buf) {
+			Err(e) => {
+				println!("Serial read error: {e}");
+				return
+			}
+			Ok(_n) => ()
+		}
+		
+		
+		const HREF_PATTERN: &[u8] = &[1, 1, 254, 254];
+		const VSYNC_PATTERN: &[u8] = &[2, 2, 253, 253];
+		let width = client_rect.width() as u32;
+		let height = client_rect.height() as u32;
+		
+		for byte in buf {
+			// RGB 565
+			if self.signal_x < width && self.signal_y < height {
+				let i = ((self.signal_y * width as u32 + self.signal_x) as usize) << 2;
+				
+				if self.first_byte_in_pair {
+					pixel_buffer[i | 0] = byte << 3;
+					pixel_buffer[i | 1] = (byte & 0b11100000) >> 3;
+					
+					self.first_byte_in_pair = false;
+				} else {
+					pixel_buffer[i | 1] |= byte << 5;
+					pixel_buffer[i | 2] = byte & 0b11111000;
+					
+					self.first_byte_in_pair = true;
+					self.signal_x += 1;
+				}
+			}
+			
+			// Raw
+			// if self.signal_x < width && self.signal_y < height {
+			// 	let color_offset = ((self.signal_x & 1) + (self.signal_y & 1)) as usize;
+			// 	let i = ((self.signal_y * width as u32 + self.signal_x) as usize) << 2;
+			// 	pixel_buffer[i | color_offset] = byte;
+			// 	self.signal_x += 1;
+			// }
+			
+			
+			if byte == VSYNC_PATTERN[self.vsync_match] {
+				self.href_match = 0;
+				self.vsync_match += 1;
+				if self.vsync_match == VSYNC_PATTERN.len() {
+					self.signal_x = 0;
+					self.signal_y = 0;
+					self.vsync_match = 0;
+					self.first_byte_in_pair = true;
+				}
+				continue
+			}
+			
+			self.vsync_match = 0;
+			
+			if byte == HREF_PATTERN[self.href_match] {
+				self.href_match += 1;
+				if self.href_match == HREF_PATTERN.len() {
+					self.signal_x = 0;
+					self.signal_y += 1;
+					self.href_match = 0;
+					self.first_byte_in_pair = true;
+				}
+				continue
+			}
+			
+			self.href_match = 0;
+			
+		}
+		
+		handle.request_redraw();
+	}
 }
 
 impl SimpleWindowApp for App {
 	fn on_init(&mut self, handle: &WindowHandle) {
 		self.try_build_menu(handle);
-		handle.set_timer(1, 10);
+		handle.set_timer(1, 20);
 	}
 	
 	fn on_command(&mut self, handle: &WindowHandle, _pixel_buffer: &mut [u8], _client_rect: &Rect, command_id: u16) {
 		match command_id {
-			0 => self.try_build_menu(handle),
+			128 => self.try_build_menu(handle),
+			129 => {
+				self.port = None;
+				self.try_build_menu(handle);
+			}
 			n => {
 				if let Some(port) = self.port.as_ref() {
 					if let Some(name) = port.name() {
-						if self.available_ports[n as usize - 1].port_name == name {
+						if self.available_ports[n as usize].port_name == name {
 							println!("Already listening to port {}", &name);
 							return;
 						}
 					}
 				}
 				
-				let port_name = &self.available_ports[n as usize - 1].port_name;
-				let port_attempt = serialport::new(port_name, 3000000)
+				let port_name = &self.available_ports[n as usize].port_name;
+				let port_attempt = serialport::new(port_name, 115200)
 					.stop_bits(serialport::StopBits::One)
 					.data_bits(serialport::DataBits::Eight)
 					.open();
@@ -115,7 +213,7 @@ impl SimpleWindowApp for App {
 					Ok(port) => {
 						self.port = Some(port);
 						let port_menu = handle.get_menu().unwrap().get_submenu(0).unwrap();
-						for i in 1..=port_menu.item_count() {
+						for i in 0..port_menu.item_count() {
 							port_menu.set_item_check(i as u16, false).unwrap();
 						}
 						port_menu.set_item_check(n, true).unwrap();
@@ -148,55 +246,9 @@ impl SimpleWindowApp for App {
 	fn on_timer(&mut self, handle: &WindowHandle, pixel_buffer: &mut [u8], client_rect: &Rect, timer_id: usize) {
 		match timer_id {
 			1 => {
-				if let Some(port) = &mut self.port {
-					match port.bytes_to_read() {
-						Ok(n) => {
-							let n = n & (!1);
-							if n > 0 {
-								let mut buf = vec![0; n as usize];
-								match port.read(&mut buf) {
-									Ok(_n) => {
-										
-										for data in buf.chunks_exact(2) {
-											match data[0] {
-												0 => {
-													let width = client_rect.width();
-													let height = client_rect.height();
-													if self.signal_x >= width as u32 || self.signal_y >= height as u32 { continue }
-													
-													let i = ((self.signal_y * width as u32 + self.signal_x) as usize) << 2;
-													
-													pixel_buffer[i | ((self.signal_x & 1) + (self.signal_y & 1)) as usize] = data[1];
-													
-													self.signal_x += 1;
-												}
-												1 => {
-													self.signal_x = 0;
-													self.signal_y += 1;
-												}
-												2 => {
-													self.signal_x = 0;
-													self.signal_y = 0;
-												}
-												_ => {
-													port.read(&mut vec![0u8; 1]).unwrap_or(0);
-												}
-											}
-										}
-										
-										handle.request_redraw();
-									}
-									Err(e) => println!("Serial read error: {e}")
-								}
-							}
-						}
-						Err(e) => println!("Serial read error: {e}")
-					}
-				}
-				
 				handle.set_timer(1, 10);
-				
-			},
+				self.render_data(handle, pixel_buffer, client_rect);
+			}
 			_ => ()
 		}
 	}
